fcd7d4dc4fd19dd7a1ab4f5a4e8c7142aaa576cc
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2021-05-16 17:19:50 +0800

    Support adding of brand in image

--- prepare_for_publishing.py
+++ prepare_for_publishing.py
@@ -5,6 +5,7 @@ import shutil
 from PIL import Image, ImageDraw, ImageFont
 from tqdm import tqdm
 
+BRAND_TEMPLATE = 'templates/brand.PNG'
 ITEM_CODE_TEMPLATE = 'templates/code.PNG'
 ITEM_SIZE_PRICE_TEMPLATE = 'templates/size_price.PNG'
 DONE_DIR = 'done'
@@ -51,8 +52,6 @@ def prepare_dir_for_publishing(dir_path):
         os.mkdir(done_dir)
 
     for index, item in tqdm(enumerate(os.listdir(dir_path), start=1)):
-        # TODO: skip if directory
-        # TODO: skip if DS_Store
         try:
             process_image(
                 dir_path,
@@ -61,7 +60,7 @@ def prepare_dir_for_publishing(dir_path):
                 index,
                 done_dir,
             )
-        except Exception as e:
+        except Exception:
             print(f'Error happened for: {item}')
 
 
@@ -81,21 +80,20 @@ def make_final_image(original_full_path, img_dict):
     return item
 
 
-def make_size_and_price_image(filename, item_height, item_width):
+def make_size_and_price_image(filename: str, item_height: int, item_width: int):
     """
     :param filename: filename with no ext
 
     Expected filename
 
-    1_M-S_200_300_400
+    1_M-S_200_220
 
     1 - Unique identifier
     M-S - Size
     200 - mine price
-    300 - steal price
-    400 - grab price
+    220 - starting bid price
     """
-    unique_identifier, size_text, *prices = filename.split('_')
+    unique_identifier, size_text, *prices, brand = filename.split('_')
 
     font_color = GRAY
     size_price_template = Image.open(ITEM_SIZE_PRICE_TEMPLATE)
@@ -123,10 +121,10 @@ def make_size_and_price_image(filename, item_height, item_width):
     for i, price in enumerate(prices):
         size_price_y += 300
 
-        if i == 2:
+        if i == 1:
             bid_font = ImageFont.truetype(AMERICAN_TYPEWRITER, size=150)
             size_price_x += 25
-            bid_text_1 = 'Open for bidding!'
+            bid_text_1 = 'Open for bidding'
             bid_text_2 = f'Starts at {price}'
             draw_size_price.text(
                 (size_price_x, size_price_y),
@@ -153,7 +151,7 @@ def make_size_and_price_image(filename, item_height, item_width):
             )
 
     size_price_dimensions = (
-        round(item_height *.25),
+        round(item_height * .25),
         round(item_width * .25),
     )
     size_price_img = size_price_template.resize(size_price_dimensions)
@@ -161,10 +159,8 @@ def make_size_and_price_image(filename, item_height, item_width):
     return size_price_img
 
 
-def make_item_code_image(item_code, item_height, item_width):
+def make_item_code_image(item_code: str, item_height: int, item_width: int):
     """
-    :param item_code: str
-
     :returns: img
     """
     font_color = GRAY
@@ -183,6 +179,32 @@ def make_item_code_image(item_code, item_height, item_width):
     code_width = round(item_width * .12)
     code_img = code_template.resize((code_width, code_height))
     return code_img
+
+
+def make_brand_image(filename: str, item_height: int, item_width: int) -> Image.Image:
+    """
+    """
+    brand = filename.split('_')[-1]
+
+    if brand == '*':
+        return
+
+    font_color = GRAY
+    brand_template = Image.open(BRAND_TEMPLATE)
+    draw_brand = ImageDraw.Draw(brand_template)
+    brand_position = 55, 260
+    brand_font = ImageFont.truetype(ARIAL, size=350)
+    draw_brand.text(
+        brand_position,
+        brand,
+        fill=font_color,
+        font=brand_font,
+    )
+    brand_height = round(item_height * .05)
+    brand_width = round(item_width * .2)
+    brand_img = brand_template.resize((brand_width, brand_height))
+
+    return brand_img
 
 
 def generate_item_code(batch, number):
@@ -218,6 +240,11 @@ def process_image(dir_path, filename, batch, number, done_dir):
         item.height,
         item.width,
     )
+    brand_img = make_brand_image(
+        filename_no_ext,
+        item.height,
+        item.width,
+    )
 
     item_copy = item.copy()
     # Add code to item
@@ -242,6 +269,14 @@ def process_image(dir_path, filename, batch, number, done_dir):
         SIZE_PRICE_COORDS,
         size_price_img.convert('RGBA'),
     )
+    if brand_img:
+        brand_x = 0
+        brand_y = item.height - brand_img.height
+        item_copy.paste(
+            brand_img,
+            (brand_x, brand_y),
+            brand_img.convert('RGBA')
+        )
 
     # Use code as filename for edited item
     final_item_code_with_ext = f'{item_code}.{ext}'
